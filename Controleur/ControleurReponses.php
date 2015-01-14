20c37a183a52137262afb4e7ee04848887e9778c
Nils Truchaud <>
2015-01-14 13:02:13 +0100

    Refactoring du ControleurReponses

--- Controleur/ControleurReponses.php
+++ Controleur/ControleurReponses.php
@@ -21,53 +21,44 @@ class ControleurReponses extends ControleurSecurise {
     public function index() {
         $answers = $this->reponse->getAnswers();
 
-        /*
-         * Pour les données sur l'utilisateur
-         */
         foreach ($answers as $answer) {
+
+            /*
+             * Pour les données sur l'utilisateur
+             */
             $idUser = $answer->get("idUser");
             $users = $this->reponse->getUser($idUser);
             $date = $users->get("ddn");
-            $age = (int)((time() - strtotime($date->format('Y-m-d'))) / 3600 / 24 / 365.242);
+            $age = (int) ((time() - strtotime($date->format('Y-m-d'))) / 3600 / 24 / 365.242);
             $mail = $users->get("email");
             $sexe = $users->get("sexe");
-        }
-        
-        /*
-         * Pour les mood pairs
-         */
 
-        foreach ($answers as $answer) {
+            /*
+             * Pour les mood pairs
+             */
             $moodPairsId = $answer->get("moodsId");
             foreach ($moodPairsId as $moodPairId) {
                 $aMoodPairName = $this->moodPairs->getAMoodPairName($moodPairId);
                 $moodPairs[$moodPairId] = $aMoodPairName;
             }
-        }
 
-        /*
-         * Pour les réponses aux mood pairs
-         */
-        foreach ($answers as $answer) {
+            /*
+             * Pour les réponses aux mood pairs
+             */
             $moodPairsValue[$answer->getObjectId()] = $answer->get("moodsValue");
             $moodPairsHeader = $answer->get("moodsValue");
-        }
 
-        /*
-         * Pour les temps montrés
-         */
-        foreach ($answers as $answer) {
+            /*
+             * Pour les temps montrés
+             */
             $displayedDuration[$answer->getObjectId()] = $answer->get("displayedDuration");
             $displayedDurationHeader = $answer->get("displayedDuration");
-        }
 
-        /*
-         * Pour les estimations de temps
-         */
-        foreach ($answers as $answer) {
+            /*
+             * Pour les estimations de temps
+             */
             $estmations[$answer->getObjectId()] = $answer->get("estimatedDuration");
         }
-
         $this->genererVue(array('moodPairsName' => $moodPairs, 'moodPairsValue' => $moodPairsValue, 'displayed' => $displayedDuration, 'estimations' => $estmations,
             'answers' => $answers, 'displayDurationHeader' => $displayedDurationHeader, 'moodPairsHeader' => $moodPairsHeader, 'age' => $age, 'mail' => $mail, 'sexe' => $sexe));
     }
